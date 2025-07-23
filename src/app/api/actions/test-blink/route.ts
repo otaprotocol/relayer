be3ac0fa8f8359e2c1cc7f497afef328af69bfc5
Efe <efe@efebehar.dev>
2025-07-23 13:43:55 +0200

    feat: blink post

--- src/app/api/actions/test-blink/route.ts
+++ src/app/api/actions/test-blink/route.ts
@@ -1,4 +1,8 @@
-import { ActionGetResponse, ACTIONS_CORS_HEADERS, BLOCKCHAIN_IDS } from "@solana/actions";
+import { ActionCodesClient } from '@actioncodes/sdk'
+import { AttachRequestSchema, AttachResponseSchema } from "@actioncodes/relayer/schemas/attach";
+import { ActionGetResponse, ActionPostResponse, ACTIONS_CORS_HEADERS, BLOCKCHAIN_IDS } from "@solana/actions";
+
+const actionCodesClient = new ActionCodesClient();
 
 const blockchain = BLOCKCHAIN_IDS.mainnet;
 
@@ -44,6 +48,63 @@ export const GET = async () => {
         status: 200,
         headers,
     });
+};
+
+export const POST = async (req: Request) => {
+    try {
+        const url = new URL(req.url);
+
+        const message = url.searchParams.get("message");
+        const action_code = url.searchParams.get("action_code");
+
+        if (!message || !action_code) {
+            return new Response(JSON.stringify({ error: "Missing required parameters" }), {
+                status: 400,
+                headers,
+            });
+        }
+
+        await actionCodesClient.attachMessage(action_code, message);
+
+
+        for await (const status of actionCodesClient.observeStatus(action_code)) {
+            if (status.status === 'finalized') {
+                const response: ActionPostResponse = {
+                    type: 'message',
+                    data: status.signedMessage ?? '',
+                    links: {
+                        next: {
+                            type: 'post',
+                            href: '/api/actions/test-blink?message={message}&action_code={action_code}',
+                        }
+                    }
+                }
+
+                return new Response(JSON.stringify(response), {
+                    status: 200,
+                    headers,
+                });
+            } else if (status.status === 'expired') {
+                return new Response(JSON.stringify({ error: "Action code expired" }), {
+                    status: 400,
+                    headers,
+                });
+            } else if (status.status === 'error') {
+                return new Response(JSON.stringify({ error: "Action code error" }), {
+                    status: 400,
+                    headers,
+                });
+            }
+        }
+
+    } catch (error) {
+        // Log and return an error response
+        console.error("Error processing request:", error);
+        return new Response(JSON.stringify({ error: "Internal server error" }), {
+            status: 500,
+            headers,
+        });
+    }
 };
 
 export const OPTIONS = async () => {
